564f7a703edbc45cdc7b321f5cb1f38713f0c741
SlyusarVladimir <130454507+SlyusarVladimir@users.noreply.github.com>
2023-12-21 16:11:48 +0300

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -134,3 +134,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Update database configuration from $DATABASE_URL environment variable (if defined)
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
